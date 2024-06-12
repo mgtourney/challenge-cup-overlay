e07db7f6ca723d1c37fe6d628e274e4bc32ffbb0
niko <niko@velvet.moe>
2024-06-11 18:26:42 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Beat Saber Challenge Cup Overlay
+
+To run dev server:
+- `pnpm dev`
+
+please don't use npm or yarn. If you don't have pnpm, follow the instructions found [here](https://pnpm.io/installation)
---
+++ src/routes/countdown/+page.svelte
@@ -0,0 +1,5 @@
+<div class="flex bg-slate-700 h-screen w-screen">
+    <div class="flex justify-center items-center h-full w-full">
+        <h1 class="text-3xl font-bold text-white">Countdown</h1>
+    </div>
+</div>
