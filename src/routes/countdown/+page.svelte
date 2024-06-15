d83dbc53aa24c60d6d248ca7fc46b6b6c32f7d08
teknoz <60395967+TeknozFR@users.noreply.github.com>
2024-06-14 22:34:52 -0400

    countdown design
    countdown design complete, missing countdown

--- src/routes/countdown/+page.svelte
+++ src/routes/countdown/+page.svelte
@@ -1,5 +1,21 @@
-<div class="flex bg-slate-700 h-screen w-screen">
-    <div class="flex justify-center items-center h-full w-full">
-        <h1 class="text-3xl font-bold text-white">Countdown</h1>
+<div class="h-screen w-screen bg-grey flex flex-col text-white justify-center">
+    <div class="flex flex-col items-center py-4">
+        <img src="/BSCC2024_Logo.png" class="h-[400px] w-[400px] justify-center opacity-65 mt-[120px]" alt="logo"/>
+    </div>
+
+    <div class="flex flex-col items-center px-3 py-3">
+        <img src="/BSCC2024_text.png" class="h-[350px] w-[751px] justify-center" alt="logo"/>
+    </div>
+
+    <div class="flex flex-col justify-end items-center h-fit w-full">
+        <p class="text-5xl font-medium">STARTING SOON!</p>
+        </div>
+
+    <div class="flex flex-grow" />
+
+    <div class="flex w-full h-fit">
+        <div class="flex flex-col justify-end items-center h-fit w-full">
+            <p class="text-2xl font-bold py-3"><span class="font-extrabold">ARTIST</span> - SONG NAME</p>
+            </div>
     </div>
 </div>
